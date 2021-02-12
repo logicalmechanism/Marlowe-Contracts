5de09c9c74002a5de498122a7110864b66efe39e
LogicalMechanism <mqparkinson@gmail.com>
2021-02-12 10:39:49 -0800

    plutus app update

--- plutus_app.hs
+++ plutus_app.hs
@@ -20,38 +20,66 @@ import           Wallet.Emulator.Wallet    (Wallet, walletPubKey)
 -}
 
 
--- Validator function has the format of (Datum -> Redeemer -> ValidatorCtx -> Bool) where it is computed
--- on the chain. If we look at the ScriptInstance, this is where we define the type of the
--- Datum and Redeemer.
-validateGuess :: Integer -> Integer -> ValidatorCtx -> Bool
-validateGuess _ _ _ = True
+-- The Validator function has the format of (Datum -> Redeemer -> ValidatorCtx -> Bool).
+-- This is computed on the chain.
+-- The Datum and Redeemer types are defined in the exampleInstance data type ExampleDataType.
+-- 
+--  This function just passes True.
+verify :: Integer -> Integer -> ValidatorCtx -> Bool
+verify _ _ _ = True
 
 
+-- The DataType describes the type of values used in the Datum and Redeemer.
+-- These two parameters are wrappers around the data we use for the input and output.
+--
+-- data ExampleDataType
+--
+-- The format is standard for each validator.
+-- 
+data LogicalDataType
+instance Scripts.ScriptType LogicalDataType where
+    type instance DatumType LogicalDataType = Integer -- Change to any allowed Haskell Type
+    type instance RedeemerType LogicalDataType = Integer -- Change to any allowed Haskell Type
 
 
--- The "lock" contract endpoint. using the LogicalSchema, we can extract the input from "lock" endpoint.
--- To submit the transaction to the network, we need (1) Script Instance (2) transaction
--- constraints, so we put restrictions on fund to be spent later by the validator
--- function, in this example the constraints will be the randomNum.
+-- The script instance contains the information about the validator script.
+-- This allows the input to be submitted to the chain. Every validator has
+-- the same form:
+--
+-- (Datum -> Redeemer -> ValidatorCtx -> Bool)
+--
+-- The Datum and Redeemer types are described in a data object. This allows
+-- a lot of creativity for input types for each endpoint. The types need
+-- to be declared inside the instance for each transaction.
+--
+-- This function describes the type of validator used to validate a tx.
+-- 
+-- @see: LogicalDataType
+-- @see: verify
+--
+logicalInstance :: Scripts.ScriptInstance LogicalDataType
+logicalInstance = Scripts.validator @LogicalDataType
+    $$(PlutusTx.compile [|| verify ||]) -- input validator function name here
+    $$(PlutusTx.compile [|| Scripts.wrapValidator @Integer @Integer ||]) -- Change @Integer to which ever type is used in the DataType
+
+
+-- The "lock" contract endpoint. using the LogicalSchema.
+-- The contract is a transaction to the blockchain.
+--
+-- exampleParams var1 var2 var3 <- endpoint @"example" @exampleParams
+-- let tx = Contraints.mustPayToTheScript uniqueID amount
+-- void $ submitTxConstraints exampleInstance tx
+--
+-- Submitting the tx constraints needs to run through a validator for the chain.
+-- In this case, logicalInstance is the container for the validator.
+--
+-- @see: logicalInstance
+--
 lock :: AsContractError e => Contract LogicalSchema e ()
 lock = do
     LockParams amount <- endpoint @"lock" @LockParams
     let tx            = Constraints.mustPayToTheScript 123 amount
-    void $ submitTxConstraints gameInstance tx
-
--- the script is taking two parameter DatumType which is a wrapper around the data we used
--- in the script output reedemerType is the wrapper around data we take as an input
-data GameDataType
-instance Scripts.ScriptType GameDataType where
-    type instance DatumType GameDataType = Integer
-    type instance RedeemerType GameDataType = Integer
-
--- this is our game instance where we take the validator script and make it part of the on-chain
--- later on we will use it as part of input of submit the transaction to the network.
-gameInstance :: Scripts.ScriptInstance GameDataType
-gameInstance = Scripts.validator @GameDataType
-    $$(PlutusTx.compile [|| validateGuess ||])
-    $$(PlutusTx.compile [|| Scripts.wrapValidator @Integer @Integer ||])
+    void $ submitTxConstraints logicalInstance tx
 
 
 -- Each endpoint needs a parameter function of this form. The deriving
@@ -67,10 +95,14 @@ data LockParams = LockParams
     deriving stock (Prelude.Eq, Prelude.Show, Generic) -- Always include
     deriving anyclass (FromJSON, ToJSON, IotsType, ToSchema, ToArgument) -- Always include
 
+
 -- The schema can consist of N endpoints. The .\/ operator combines the
 -- exposed endpoints. Each endpoint has a parameter function.
 --
--- .\/ Endpoint "firstOption" firstOptionParams
+-- .\/ Endpoint "exampleOption" exampleOptionParams
+--
+-- @see: lock
+-- @see: LockParams
 --
 type LogicalSchema =
     BlockchainActions
@@ -80,7 +112,9 @@ type LogicalSchema =
 -- Use select to create two user inputs. This allows the app to proceed with
 -- whichever option receives the input first.
 --
--- endpoints firstOption `select` secondOption
+-- endpoints exampleOption `select` secondOption
+--
+-- @see: lock
 --
 endpoints :: AsContractError e => Contract LogicalSchema e ()
 endpoints = lock
@@ -89,6 +123,8 @@ endpoints = lock
 -- Bind everything to a schema definition for the application.
 --
 -- mkSchemaDefinitions ''exampleSchema
+--
+-- @see: LogicalSchema
 --
 mkSchemaDefinitions ''LogicalSchema
 $(mkKnownCurrencies [])
