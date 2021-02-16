6a7bcc42e45305ae22aaff1d87bd06d262db7057
LogicalMechanism <mqparkinson@gmail.com>
2021-02-16 10:19:41 -0800

    basic lottery update

--- plutus_lottery.hs
+++ plutus_lottery.hs
@@ -1,4 +1,4 @@
--- A wallet starts a schedule collection which allows N wallets to contribute k ADA.
+-- A wallet starts a start lottery which allows N wallets to contribute k ADA.
 -- At the then of a predefined period a random wallet is paid all ADA in the lottry.
 --
 import           Control.Applicative                  (Applicative (pure))
@@ -30,12 +30,24 @@ import qualified Wallet.Emulator                      as Emulator
 import Numeric
 import qualified Data.ByteString.Char8     as C
 import Ledger.AddressMap
+import System.Random
 
 
--- Temp rando number generator
---
-randomNumber :: Integer -> Integer
-randomNumber players = (toInteger $ fromEnum $ C.last $ C.pack $ show $ Ledger.pubKeyHash ((Emulator.walletPubKey (Emulator.Wallet players)))) `mod` players
+-- | DEFAULT Parameters for the Lottery. Ends in 50 Slots with 5 players.
+theLottery :: Lottery
+theLottery = Lottery
+    { lotteryDeadline = 50
+    , lotteryCollectionDeadline = 60
+    , lotteryPlayers = [(player x) | x <- [1..5] ]
+    }
+
+-- Creates a pubkeyhask for a wallet.
+player :: Integer -> PubKeyHash
+player id = (pubKeyHash $ Emulator.walletPubKey (Emulator.Wallet id))
+
+-- RNG betwen lo and hi.
+getRn :: (RandomGen g) => Integer -> Integer -> g -> (Integer, g)
+getRn lo hi g = randomR (lo, hi) g
 
 -- number of players stored in the lottery.
 --
@@ -43,99 +55,76 @@ numberOfPlayers :: [PubKeyHash] -> Integer
 numberOfPlayers [] = 0
 numberOfPlayers users =  1 + (numberOfPlayers (tail users))
 
--- | A crowdfunding campaign.
-data Campaign = Campaign
-    { campaignDeadline           :: Slot
-    -- ^ The date by which the campaign target has to be met
-    , campaignCollectionDeadline :: Slot
-    -- ^ The date by which the campaign owner has to collect the funds
-    , campaignPlayers              :: [PubKeyHash]
-    -- ^ This key is entitled to retrieve the
-    --   funds if the campaign is successful.
+-- | A lottery system.
+data Lottery = Lottery
+    { lotteryDeadline           :: Slot
+    , lotteryCollectionDeadline :: Slot
+    , lotteryPlayers              :: [PubKeyHash]
     } deriving (Generic, ToJSON, FromJSON, ToSchema)
 
-PlutusTx.makeLift ''Campaign
+PlutusTx.makeLift ''Lottery
 
--- | Action that can be taken by the participants in this contract. 
-data CampaignAction =  Pass
-
-PlutusTx.makeIsData ''CampaignAction
-PlutusTx.makeLift ''CampaignAction
-
-type CrowdfundingSchema =
+-- Endpoint schema
+type LotterySchema =
     BlockchainActions
-        .\/ Endpoint "schedule collection" ()
+        .\/ Endpoint "start lottery" ()
         .\/ Endpoint "contribute" ()
 
-data Crowdfunding
-instance Scripts.ScriptType Crowdfunding where
-    type instance RedeemerType Crowdfunding = CampaignAction
-    type instance DatumType Crowdfunding = PubKeyHash
+-- | Start the lottery or play the lottry.
+lotterysystem :: AsContractError e => Lottery -> Contract LotterySchema e ()
+lotterysystem c = contribute c `select` startLottery c
 
-scriptInstance :: Campaign -> Scripts.ScriptInstance Crowdfunding
-scriptInstance cmp = Scripts.validator @Crowdfunding
+-- data
+data LotteryData
+instance Scripts.ScriptType LotteryData where
+    type instance RedeemerType LotteryData = ()
+    type instance DatumType LotteryData = ()
+
+scriptInstance :: Lottery -> Scripts.ScriptInstance LotteryData
+scriptInstance cmp = Scripts.validator @LotteryData
     ($$(PlutusTx.compile [|| mkValidator ||]) `PlutusTx.applyCode` PlutusTx.liftCode cmp)
     $$(PlutusTx.compile [|| wrap ||])
     where
-        wrap = Scripts.wrapValidator @PubKeyHash @CampaignAction
-
+        wrap = Scripts.wrapValidator @() @()
 
 -- | The validator script.
-mkValidator :: Campaign -> PubKeyHash -> CampaignAction -> ValidatorCtx -> Bool
-mkValidator c con act p = case act of
-    Pass    -> True
-
--- | Start the lottery or play the lottry.
-crowdfunding :: AsContractError e => Campaign -> Contract CrowdfundingSchema e ()
-crowdfunding c = contribute c `select` scheduleCollection c
-
--- | DEFAULT
-theCampaign :: Campaign
-theCampaign = Campaign
-    { campaignDeadline = 50
-    , campaignCollectionDeadline = 60
-    , campaignPlayers = [(player x) | x <- [1..5] ]
-    }
-
--- Creates a pubkeyhask for a wallet.
-player :: Integer -> PubKeyHash
-player id = (pubKeyHash $ Emulator.walletPubKey (Emulator.Wallet id))
+mkValidator :: Lottery -> () -> () -> ValidatorCtx -> Bool
+mkValidator c _ _ p = True
 
 
 -- | Each contributer adds one ada into the lottery.
-contribute :: AsContractError e => Campaign -> Contract CrowdfundingSchema e ()
+contribute :: AsContractError e => Lottery -> Contract LotterySchema e ()
 contribute cmp = do
     () <- endpoint @"contribute"
     contributor <- pubKeyHash <$> ownPubKey
-    -- add in the a new pubkey hash into the campaign here
     let inst   = scriptInstance cmp
-        tx     = Constraints.mustPayToTheScript contributor (Ada.lovelaceValueOf 1)
-                <> Constraints.mustValidateIn (Ledger.interval 1 (campaignDeadline cmp))
+        tx     = Constraints.mustPayToTheScript () (Ada.lovelaceValueOf 1)
+                <> Constraints.mustValidateIn (Ledger.interval 1 (lotteryDeadline cmp))
     txid <- fmap txId (submitTxConstraints inst tx)
-    utxo <- watchAddressUntil (Scripts.scriptAddress inst) (campaignCollectionDeadline cmp)
+    utxo <- watchAddressUntil (Scripts.scriptAddress inst) (lotteryCollectionDeadline cmp)
     if Constraints.modifiesUtxoSet tx
     then void (submitTxConstraintsSpending inst utxo tx)
     else pure ()
 
 -- | When the end slot is reached, pay a random wallet.
-scheduleCollection :: AsContractError e => Campaign -> Contract CrowdfundingSchema e ()
-scheduleCollection cmp = do
+startLottery :: AsContractError e => Lottery -> Contract LotterySchema e ()
+startLottery cmp = do
     let inst = scriptInstance cmp -- pass campaign into instance
-    ()             <- endpoint @"schedule collection"
-    _              <- awaitSlot (campaignDeadline cmp) -- wait til slot then proceed
+    ()             <- endpoint @"start lottery"
+    _              <- awaitSlot (lotteryDeadline cmp) -- wait til slot then proceed
     unspentOutputs <- utxoAt (Scripts.scriptAddress inst)
     winner         <- pubKeyHash <$> ownPubKey
-    let value = foldMap (Validation.txOutValue . Tx.txOutTxOut . snd) (Map.toList unspentOutputs)
-        tx    = Typed.collectFromScript unspentOutputs Pass
-                <> Constraints.mustPayToPubKey winner (Ada.toValue 1) -- Send back lottery creation ADA
-                <> Constraints.mustPayToPubKey ((campaignPlayers cmp) !! (randomNumber (numberOfPlayers (campaignPlayers cmp)))) value
+    let (randNum, nextGen) = getRn 1 (numberOfPlayers (lotteryPlayers cmp)) (mkStdGen 11235)
+        value              = foldMap (Validation.txOutValue . Tx.txOutTxOut . snd) (Map.toList unspentOutputs)
+        tx                 = Typed.collectFromScript unspentOutputs ()
+                            <> Constraints.mustPayToPubKey winner (Ada.toValue 1) -- Send back lottery creation ADA
+                            <> Constraints.mustPayToPubKey ((lotteryPlayers cmp) !! (randNum - 1)) value
     void $ submitTxConstraintsSpending inst unspentOutputs tx
 
 
+endpoints :: AsContractError e => Contract LotterySchema e ()
+endpoints = lotterysystem theLottery
 
-endpoints :: AsContractError e => Contract CrowdfundingSchema e ()
-endpoints = crowdfunding theCampaign
-
-mkSchemaDefinitions ''CrowdfundingSchema
+mkSchemaDefinitions ''LotterySchema
 
 $(mkKnownCurrencies [])
